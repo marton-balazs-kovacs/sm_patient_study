e38c15bca2e8c78fc19dfa960c1ee227660c8f91
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2022-06-07 15:58:21 +0200

    updated analysis

--- R/utils.R
+++ R/utils.R
@@ -10,8 +10,9 @@ count_and_prop <- function(df, var_name) {
 
 '%ni%' <- Negate('%in%')
 
-plot_count_and_prop <- function(df, var_name) {
-  df %>% 
+plot_count_and_prop <- function(df, var_name, theme_apa = TRUE, x_title = NULL) {
+  plot <-
+    df %>% 
     count_and_prop({{var_name}}) %>%
     # arrange(desc(percentage)) %>% 
     mutate({{var_name}} := as.factor({{var_name}})) %>%
@@ -21,7 +22,26 @@ plot_count_and_prop <- function(df, var_name) {
       y = percentage
     ) +
     ggplot2::geom_bar(stat = "identity") +
-    ggplot2::scale_y_continuous(expand = c(0,0), limits = c(0, 100), labels = scales::label_percent(scale = 1)) +
-    ggplot2::labs(y = "Percentage") +
-    papaja::theme_apa()
+    ggplot2::scale_y_continuous(expand = c(0, 0), limits = c(0, 100), labels = scales::label_percent(scale = 1)) +
+    ggplot2::labs(
+      y = "Percentage"
+      )
+  
+  if(theme_apa) {
+    plot <-
+      plot +
+      papaja::theme_apa()
+  } else {
+    plot <-
+      plot +
+      theme_classic()
+  }
+  
+  if(!is.null(x_title)) {
+    plot <-
+      plot + 
+      ggplot2::labs(x = x_title)
+  }
+  
+  plot
 }
